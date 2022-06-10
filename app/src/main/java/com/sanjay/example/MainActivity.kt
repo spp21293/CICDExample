9f5995cce43ea73e056c9361e1cc01a333daa752
sanjay.lohar <sanjay.lohar@brainvire.com>
2022-06-10 11:29:09 +0530

    Add feature 1

--- app/src/main/java/com/sanjay/example/MainActivity.kt
+++ app/src/main/java/com/sanjay/example/MainActivity.kt
@@ -2,10 +2,26 @@ package com.sanjay.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nameEditText: EditText
+    lateinit var sayHelloButton: Button
+    lateinit var greetingTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.helloButton)
+        greetingTextView = findViewById(R.id.greetingTextView)
+
+        sayHelloButton.setOnClickListener {
+            greetingTextView.text = "Hello ${nameEditText.text}"
+        }
     }
 }
